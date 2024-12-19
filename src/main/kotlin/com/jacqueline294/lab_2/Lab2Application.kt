c632ee9974d6374a4f3ed25e1f82cca574060125
jacqueline294 <144172392+jacqueline294@users.noreply.github.com>
2024-12-19 09:43:34 +0100

    first commit

---
+++ src/main/kotlin/com/jacqueline294/lab_2/Lab2Application.kt
@@ -0,0 +1,11 @@
+package com.jacqueline294.lab_2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab2Application
+
+fun main(args: Array<String>) {
+	runApplication<Lab2Application>(*args)
+}
---
+++ src/test/kotlin/com/jacqueline294/lab_2/Lab2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jacqueline294.lab_2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
